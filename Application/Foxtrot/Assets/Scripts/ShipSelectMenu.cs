914c8aed06c16f0637efe9e66927be8e438d6ee5
hiteda <hiteda@onid.oregonstate.edu>
2016-10-26 22:22:49 -0700

    Add progress bar to ship selection menu

--- Application/Foxtrot/Assets/Scripts/ShipSelectMenu.cs
+++ Application/Foxtrot/Assets/Scripts/ShipSelectMenu.cs
@@ -1,40 +1,64 @@
-﻿using UnityEngine;
+﻿/******************************************************************
+ * File:            ShipSelectMenu.cs
+ * Author:          David Hite
+ * Date Created:    10/19/2016
+ * Date Modified:   10/26/2016
+ * Description:
+ * Contains the ShipSelectMenu class, which controls the ship 
+ * selection menu
+ ******************************************************************/
+using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class ShipSelectMenu : MonoBehaviour {
+  public Slider m_ProgressSlider;
+  private Canvas m_ProgressCanvas;
+
   void Start()
   {
     Player.instance.SetVisible(false);
+    m_ProgressSlider = GetComponentInChildren<Slider>();
+    m_ProgressCanvas = GetComponentsInChildren<Canvas>()[1];
+    HideSlider(true);
   }
+
+  /*************************************************/
+  /** LoadScene
+  Assigns the selected ship to the player and loads
+  the first level
+  @param  : ship  int used as enum of the selected
+                  ship
+  **************************************************/
   public void LoadScene(int ship)
   {
     Player.instance.SetShip((Player.EShip)ship);
     var result = SceneManager.LoadSceneAsync(1);
     result.allowSceneActivation = true;
+    HideSlider(false);
     while (result.progress < 0.9f)
     {
       // Play loading animation
+      m_ProgressSlider.value = result.progress;
     }
     Player.instance.SetVisible(true);
   }
-	// Use this for initialization
-	/*void Start () {
-    GetComponent<Renderer>().material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
-  }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-  void OnMouseEnter()
+  /*************************************************/
+  /** HideSlider
+  Hides or shows the progress slider bar
+  @param  : isHidden  When true, moves the slider
+                      bar out of view. Otherwise
+                      moves slider to center of screen
+  **************************************************/
+  private void HideSlider(bool isHidden)
   {
-    GetComponent<Renderer>().material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+    var position = m_ProgressCanvas.transform.position;
+    if (isHidden)
+      position.y = 1000f;
+    else
+      position.y = 346.5f;
+    m_ProgressCanvas.transform.position = position;
   }
-
-  void OnMouseExit()
-  {
-    GetComponent<Renderer>().material.shader = Shader.Find("Diffuse");
-  }*/
 }
