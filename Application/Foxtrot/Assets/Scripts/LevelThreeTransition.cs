584c091012ade9a6cf1510bf243a4681d5dea277
KelbyFaessler <kmfaessler@gmail.com>
2016-12-02 22:29:38 -0600

    lvl 3 transition, enemy tweaks

---
+++ Application/Foxtrot/Assets/Scripts/LevelThreeTransition.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelThreeTransition : MonoBehaviour {
+
+  // Use this for initialization
+  void Start()
+  {
+    HUDCanvas.instance.gameObject.SetActive(false);
+    Invoke("LoadLevelThree", 3.0f);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void LoadLevelThree()
+  {
+    var result = SceneManager.LoadSceneAsync("LevelThree");
+    result.allowSceneActivation = true;
+    Player.instance.SetVisible(true);
+    HUDCanvas.instance.gameObject.SetActive(true);
+  }
+}
